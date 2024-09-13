8cb381ddb8ff163a6aaa6addffff9c2c09edfdcb
CompetitionSoo <geem1282@gmail.com>
2024-09-13 10:34:47 +0900

    모듈화해서 수정하는 부분이 있길래 써봤어

--- main.py
+++ main.py
@@ -1,89 +1,16 @@
-"""
-# ch 4.2.1 main.py
-import sys # 시스템 제어 관련 모듈
+# ch 5.2.1. main.py
 
-# 위젯이란 : GUI 프로그램에서 구성요소를 뜻하는 용어
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox)
+import sys
+from ui import View
+from ctrl import Control
+from PyQt5.QtWidgets import QApplication        
 
-from PyQt5.QtGui import QIcon # Icon을 추가하기 위한 라이브러리
-# 나는 계산기 유형을 직접 정의한다. 이때 Qwidget에 기반을 둔다
-class Calculator(QWidget) :
+def main():
+    calc = QApplication(sys.argv)
+    app=QApplication(sys.argv)
+    view=View()
+    Control(view=view)
+    sys.exit(app.exec_())
 
-    def __init__(self) :
-        super().__init__() # 뭔가에 기반을 들 경우 써줘야하는 코드
-        self.initUI()
-
-    def initUI(self) :
-        self.btn1=QPushButton('Message', self)
-        self.btn1.clicked.connect(self.activateMessage) # 버튼 클릭시 핸들러 함수 연결
-
-        vbox = QVBoxLayout()
-        vbox.addStretch(1)
-        vbox.addWidget(self.btn1) # 버튼위치
-        vbox.addStretch(1)
-        
-        self.setLayout(vbox)
-
-        self.setWindowTitle('Calculator')
-        self.setWindowIcon(QIcon('icon.png'))
-        self.resize(256,256)
-        self.show()
-
-        def activateMessage(self) :
-            QMessageBox.information(self,"information" , "Button clicked!")
-
-# 클래스를 정의했으니, 여기에서 실행하겠다... 라는 실행부!
-# if__name__=='__main__' : 이 모듈이 직접 실행되는 경우
-if __name__=='__main__' :
-    app = QApplication(sys.argv) # Qt 프로그램 실행코드
-    view = Calculator()  # 내가만든 계산기 GUI 생성코드
-    sys.exit(app.exec_()) # 계산기 종료시 시스템 종료 명령
-"""
-# ch 4.2.3 main.py
-import sys # 시스템 제어 관련 모듈 
-
-# 위젯이란 : GUI 프로그램에서 구성요소를 뜻하는 용어
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout,
-                             QMessageBox, QPlainTextEdit)
-from PyQt5.QtGui import QIcon
-
-# 나는 계산기 유형을 직접 정의한다! 이때, QWidget에 기반을 둔다
-class Calculator(QWidget) :
-
-    def __init__(self) :
-        super().__init__() # 뭔가에 기반을 둘 겨우 써줘야 하는 코드
-        self.initUI()
-    
-    def initUI(self) :
-
-        self.te1 = QPlainTextEdit()
-        self.te1.setReadOnly(True)
-
-        self.btn1 = QPushButton('Message', self)
-
-        # 이벤트 핸들링 : 클릭했을 때, 뭐를 할거다! 라고 정하는 것. 
-        self.btn1.clicked.connect(self.activateMessage)
-
-        # 레이아웃 설정 
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.te1)
-        vbox.addWidget(self.btn1)
-        vbox.addStretch(1)
-
-        self.setLayout(vbox) # 설정 적용 
-
-        self.setWindowTitle('Calculator')
-        self.setWindowIcon(QIcon("icon.png"))
-        self.resize(256, 256)
-        self.show()
-
-    def activateMessage(self) :
-        # QMessageBox.information(self, "infomation", "Button Clicked!")
-        self.te1.appendPlainText("Hello, PyQt App!")
-
-# 클래스를 정의했으니, 여기에서 실행하겠다... 라는 실행부! 
-# if __name__ == "__main__" : 이 모듈이 직접 실행되는 경우 
-if __name__ == "__main__" :
-    app = QApplication(sys.argv) # Qt 프로그램 실행코드
-    view = Calculator() # 내가 만든 계산기 GUI 생성 코드 
-    sys.exit(app.exec_()) # 계산기 종료 시 시스템 종료 명령 
+if __name__ == '__main__':
+    main()
